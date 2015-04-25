d3e36a74945311bf14d1f5315582f0c780a7eff9
SpenSer Bray <matthewqa@gmail.com>
2015-04-25 15:05:08 -0500

    Validate user information requirements.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+	has_many :articles
+
+	validates :handle, presence: true,
+	                     length: { minimum: 6 }
+	validates :password, presence: true,
+	                       length: { minimum: 8 }
+	validates :email, presence: true
 end
