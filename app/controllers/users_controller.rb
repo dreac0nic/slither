10475a8b686424b3d19d57ffa0e12ec3d05b7c29
SpenSer Bray <matthewqa@gmail.com>
2015-04-25 16:30:20 -0500

    Update controller to remove simple debug notice.
    Also save the model when done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,11 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new user_params
 
-		redirect_to root_url, :notice => "I swear it worked. ;)"
-
-		#if @user.save
-		#	redirect_to root_url, :notice => "Account created successfully! Welcome!"
-		#else
-		#	render "new"
-		#end
+		if @user.save
+			redirect_to root_url, :notice => "Account created successfully! Welcome!"
+		else
+			render "new"
+		end
 	end
 
 	private
