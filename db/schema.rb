8ac2e6a5cfe112cbed7e1f8bcef2b0bc55f67db9
SpenSer Bray <matthewqa@gmail.com>
2015-04-25 15:05:55 -0500

    Attach migration schema! Whoops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425194710) do
+ActiveRecord::Schema.define(version: 20150425195502) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "handle"
