79ff507312452034c7fe6fd6659d5d96b060b2e8
SpenSer Bray <matthewqa@gmail.com>
2015-04-28 13:41:01 -0500

    Migrate the database with runs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426201940) do
+ActiveRecord::Schema.define(version: 20150428183954) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "body"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20150426201940) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "runs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "score"
+    t.time     "duration"
+    t.integer  "dots"
+    t.boolean  "completed"
+    t.datetime "finished_on"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "handle"
