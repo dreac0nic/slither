a848f8f7e787a5037e71cfe11e7db11187ad8dfc
SpenSer Bray <matthewqa@gmail.com>
2015-04-30 05:20:10 -0500

    Setup highscore table!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @new_post = Post.new
     @posts = Post.all.order("created_at desc")
+    @highscores = Run.all.order("score desc")
   end
 
   def game
